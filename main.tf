35fdc772839c1185630ddf24d2a58a8322c293c7
lalityenni <lalit.yenni@gmail.com>
2025-08-13 12:41:21 -0700

    fix: replace broken main.tf with correct network+vm config

--- main.tf
+++ main.tf
@@ -1,1 +1,143 @@
-<PASTE THE ABOVE CONTENT HERE>
+locals {
+  name_prefix = var.project_name
+  tags = {
+    project = var.project_name
+    owner   = "lalityenni"
+    env     = var.environment
+  }
+}
+
+resource "random_integer" "suffix" {
+  min = 1000
+  max = 9999
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.name_prefix}-${random_integer.suffix.result}"
+  location = var.location
+  tags     = local.tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-${local.name_prefix}"
+  address_space       = ["10.10.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-${local.name_prefix}"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.10.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-${local.name_prefix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags
+}
+
+# NSG Rules
+resource "azurerm_network_security_rule" "ssh" {
+  name                        = "allow-ssh"
+  priority                    = 1001
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "22"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "http" {
+  name                        = "allow-http"
+  priority                    = 1002
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+# Correct association resource type
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Public IP for VM
+resource "azurerm_public_ip" "pip" {
+  name                = "pip-${local.name_prefix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  tags                = local.tags
+}
+
+# NIC for VM
+resource "azurerm_network_interface" "nic" {
+  name                = "nic-${local.name_prefix}"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+
+  tags = local.tags
+}
+
+# Linux VM
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "vm-${local.name_prefix}"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.admin_username
+  network_interface_ids = [azurerm_network_interface.nic.id]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(pathexpand(var.admin_ssh_public_key))
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  # Nginx install
+  custom_data = base64encode(<<EOF
+#cloud-config
+package_update: true
+packages:
+  - nginx
+runcmd:
+  - systemctl enable nginx
+  - systemctl start nginx
+EOF
+)
+
+  tags = local.tags
+}
