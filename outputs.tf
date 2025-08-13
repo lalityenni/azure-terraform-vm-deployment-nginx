144ee6364ae96700770d4675d33d8fdf325006ce
lalityenni <lalit.yenni@gmail.com>
2025-08-13 12:48:00 -0700

    fix: correct outputs to match public_ip resource name pip

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,7 @@
 output "vm_public_ip" {
-  description = "Public IP to reach Nginx"
-  value       = azurerm_public_ip.public_ip.ip_address
+  value = azurerm_public_ip.pip.ip_address
 }
 
 output "ssh_command" {
-  description = "Convenience SSH command"
-  value       = "ssh ${var.admin_username}@${azurerm_public_ip.public_ip.ip_address}"
+  value = "ssh ${var.admin_username}@${azurerm_public_ip.pip.ip_address}"
 }
